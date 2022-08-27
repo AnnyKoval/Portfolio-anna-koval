7558a8a6bb5b89378d60618e5986f3051d4e1f76
Anna Koval <106483373+AnnyKoval@users.noreply.github.com>
2022-08-27 22:23:22 +0300

    added contact form

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ function displaySlides(n) {
   }
   slides[slide_index - 1].style.display = "block";
 }
+//contactsdocument
+document.querySelector("form").addEventListener("submit", handleSubmit);
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  let myForm = document.getElementById("pizzaOrder");
+  let formData = new FormData(myForm);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => console.log("Form successfully submitted"))
+    .catch((error) => alert(error));
+};
